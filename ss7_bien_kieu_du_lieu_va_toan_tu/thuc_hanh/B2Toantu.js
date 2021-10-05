5cd5ccde1e013a93c13c9aa4edca19cf4eb907f7
DAOBUIXUANTHANG <inorganon@gmail,com>
2021-10-05 17:18:07 +0700

    up bai

---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/B2Toantu.js
@@ -0,0 +1,8 @@
+let inputwidth
+let inputheight
+inputwidth = prompt("Nhap chieu dai: ");
+inputheight = prompt("Nhap chieu cao: ");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("dien tich = "+ area)
---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/TinhDienTichVaChuViHinhTron.js
@@ -0,0 +1,7 @@
+let radius
+radius = prompt("nhập bán kính đường tròn: ");
+r = parseInt(radius);
+area = r * r * 3.14 ;
+perimeter = r * 2 * 3.14;
+document.write("diện tích hình tròn là: ");
+document.write("chu vi hình tròn là : ");
